6f79528bfd252f0e54a2300eb9c8f8edf5dd4a17
Grzegorz Gebala <grzegorz.gebala@yahoo.com>
2019-01-03 14:04:38 +0100

    Final commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '571865335629-rf701781b9fdn9dq4h8aecgn9d0btc7t.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '5BwwYtucwDj7AwMxhzMDA4dl',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
